1ae833bd6645564782952792c4241cc3f6b54f45
Özge ÇAM <ozgecam@hotmail.com>
2024-06-14 23:35:46 +0300

    api'ye post eklendi

--- test_api.py
+++ test_api.py
@@ -1,4 +1,4 @@
-import pytest
+
 import requests
 import json
 
@@ -15,7 +15,13 @@ class Test_api_post():
     response = requests.post(url,data=json.dumps(data),headers=headers)
     assert response.status_code == 201 #HTML Locator karşılaştırması yapar
     print(response.text)  # API'nin döndürdüğü yanıtı yazdır
+    assert response.status_code == 201
 
+    response_data = response.json()  # JSON yanıtını işle
+    actual_email = response_data.get('email')  # Örnek olarak email'i kontrol edelim
+    expected_email = "3ATESTozge.cam@outlook.com"
+
+    assert actual_email == expected_email, f"Expected email: {expected_email}, Actual email: {actual_email}"
     
 #Kullanıcı , Login işlemi yapılır.
   def test_api_login(self):
@@ -30,6 +36,15 @@ class Test_api_post():
         assert response.status_code == 200
         print(response.json())
 
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        assert response.status_code == 200
+        response_data = response.json()  # JSON yanıtını işle
+        actual_email = response_data.get('email')  # Örnek olarak email'i kontrol edelim
+        expected_email = "3ATESTozge.cam@outlook.com"
+
+        assert actual_email == expected_email, f"Expected email: {expected_email}, Actual email: {actual_email}"
+
   def test_kayiteposta_post(self):
     url = "http://localhost:60805/api/Auth/Register"
     headers ={"Content-Type":"application/json"} 
@@ -41,7 +56,35 @@ class Test_api_post():
                      "phoneNumber":"05356253635"}
     response = requests.post(url,data=json.dumps(data),headers=headers)
     assert response.status_code == 500 #HTML Locator karşılaştırması yapar
-    print(response.text)  # API'nin döndürdüğü yanıtı yazdır        
+    print(response.text)  # API'nin döndürdüğü yanıtı yazdır    
+
+
+   # Kullanıcı ad alanını boş geçer
+  def api_post_ad(self):
+    url = "http://localhost:60805/api/Auth/Register"
+    headers ={"Content-Type":"application/json"} 
+    data = {"user": {"email":"pair2@outlook.com",
+                     "password":"Passw0rd!"},
+                     "fistName":"",
+                     "lastName":"CAM",
+                     "nationalIdentity":"41149488501",
+                     "phoneNumber":"05356253635"} 
+    response = requests.post(url,data=json.dumps(data),headers=headers)
+    assert response.status_code == 400 #Bad Request
+    print(response.text)  # API'nin döndürdüğü yanıtı yazdır       
+
+  def api_post_soyad(self):
+    url = "http://localhost:60805/api/Auth/Register"
+    headers ={"Content-Type":"application/json"} 
+    data = {"user": {"email":"pair2@outlook.com",
+                     "password":"Passw0rd!"},
+                     "fistName":"",
+                     "lastName":"CAM",
+                     "nationalIdentity":"41149488501",
+                     "phoneNumber":"05356253635"} 
+    response = requests.post(url,data=json.dumps(data),headers=headers)
+    assert response.status_code == 400 #Bad Request
+    print(response.text)  # API'nin döndürdüğü yanıtı yazdır      
 
 
 
